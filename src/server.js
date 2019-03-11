60b78f7a20c56ebfc011fc0abaa77fb2720949ca
Austin Slominski <austin.slominski@gmail.com>
2019-03-11 15:24:16 -0600

    styled video classification example

--- src/server.js
+++ src/server.js
@@ -4,16 +4,8 @@ const path = require('path');
 const app = express();
 app.use(express.static('src/public'));
 
-app.get('/image_classification',(req,res)=>{
-  res.sendFile('imageclassification.html', { root: __dirname + "/public"});
-});
-
-app.get('/video_classification',(req,res)=>{
-  res.sendFile('videoclassification.html', { root: __dirname + "/public"});
-});
-
-app.get('/lstm',(req,res)=>{
-  res.sendFile('lstm.html', { root: __dirname + "/public"});
+app.get('/', (req,res)=>{
+  res.sendFile('index.html');
 });
 
 app.listen(3000,()=>console.log("listening on port 3000"));
